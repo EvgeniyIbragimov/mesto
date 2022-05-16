64d174864fe5abd23a81e4e74815685e0a1e5ffd
Evgeniy Ibragimov <eugen.ibragimov@yandex.ru>
2022-05-16 20:11:30 +0300

    style: добавить стили для главной страницы

---
+++ utils/utils.js
@@ -0,0 +1,22 @@
+function openCardPreview(popup) {
+    openPopup(popup);
+  }
+  
+  function openPopup(popup) {
+    popup.classList.add('popup_is-opened');
+    document.addEventListener('keydown', escapeListener);
+  }
+  
+  function closePopup(popup) {
+    popup.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', escapeListener);
+  }
+  
+  function escapeListener(evt) {
+    if (evt.key === 'Escape') {
+      const popupIsOpened = document.querySelector('.popup_is-opened');
+      closePopup(popupIsOpened);
+    }
+  }
+  
+  export { openPopup, closePopup, openCardPreview };
